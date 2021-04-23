91843109952d1c6fb0b1e8e8a7741e79961eb938
Takafumi Arakaki <aka.tkf@gmail.com>
2021-04-23 02:20:26 -0400

    Add ci_tapir

--- src/TapirDevUtils.jl
+++ src/TapirDevUtils.jl
@@ -1,5 +1,19 @@
 module TapirDevUtils
 
-# Write your package code here.
+export ci_tapir, @ci_tapir
+
+using InteractiveUtils: code_typed, gen_call_with_extracted_types_and_kwargs
+
+function ci_tapir(f, types)
+    @nospecialize
+    (ci,), = code_typed(f, types)
+    mch = Base._which(Base.tuple_type_cons(typeof(f), types))
+    mi = Core.Compiler.specialize_method(mch.method, mch.spec_types, mch.sparams)
+    return Core.Compiler.lower_tapir(mi, ci)
+end
+
+macro ci_tapir(args...)
+    gen_call_with_extracted_types_and_kwargs(__module__, ci_tapir, args)
+end
 
 end
